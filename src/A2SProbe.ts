448ac2c55bf99a1ca6eb8eb16ee98f63170deed9
Patrick <privat@skyraptor.eu>
2022-10-22 02:47:53 +0000

    Add layer to index mapping

--- src/A2SProbe.ts
+++ src/A2SProbe.ts
@@ -5,11 +5,17 @@ import PromClient from "./PromClient";
 
 export default class A2SProbe
 {
-    interval: NodeJS.Timeout;
+    layerList?: Array<string>;
+    interval?: NodeJS.Timeout;
 
     constructor() {
-        /* Start probing the servers A2S info endpoint */
-        this.interval = setInterval(this.probe, 1000 * 15);
+        /* Load list of layers */
+        this.fetchLayerList().then(layerList => {
+            this.layerList = layerList;
+
+            /* Start probing the servers A2S info endpoint */
+            this.interval = setInterval(this.probe, 1000 * 15);
+        })
     }
 
     probe = async () => {
@@ -17,10 +23,25 @@ export default class A2SProbe
             type: 'squad',
             host: `${process.env.SQUAD_QUERY_HOST}:${process.env.SQUAD_QUERY_PORT}`
         });
-          
+        
+        /* Add player count statistic */
         container.get<PromClient>(IoCTypes.PromClient).gaugePlayers.set({type: 'Players'}, result.players.length);
-        container.get<PromClient>(IoCTypes.PromClient).gaugeMaps.set({type: 'Players'}, result.map);
+
+        /* Add layer statistic */
+        container.get<PromClient>(IoCTypes.PromClient).gaugeMaps.set({type: 'Layer'}, this.layerList?.indexOf(result.map) ?? -1);
       
         console.log(`[${(new Date()).toISOString()}][a2s] Successfully queried server!`);
+    }
+
+    async fetchLayerList() {
+        /* Load static layer list from repository, each layer has its fixed order */
+        const response = await fetch('https://raw.githubusercontent.com/Deutsche-Squad-Gemeinschaft/gists/master/data/LoadedLayersList.cfg');
+
+        /* Get each line as a layer, omit empty */
+        const result = (await response.text()).split('\n').filter(layer => layer.length);
+
+        console.log(`[${(new Date()).toISOString()}][a2s] Successfully loaded layer list!`);
+
+        return result;
     }
 }
--- src/ioc/Container.ts
+++ src/ioc/Container.ts
@@ -1,10 +1,6 @@
 import 'reflect-metadata';
 import { Container } from "inversify";
-import { IoCTypes } from './IoCTypes';
-import PromClient from '../PromClient';
 
 const container = new Container();
-
-container.bind<PromClient>(IoCTypes.PromClient).to(PromClient).inSingletonScope();
 
 export default container;
